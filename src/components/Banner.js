0d4f97b657e31018536cd028db6f3236099da7d7
pedroplm <pedropalomo.ti@gmail.com>
2023-04-21 18:56:03 -0300

    terminei o banner

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -4,48 +4,61 @@ import React from "react";
 import Image from "../assets/profile-pic.jpg";
 
 //icons
-//import { FaGithub, FaYoutube, FaDribble } from "react-icons/fa";
+import { FaGithub, FaFolderOpen, FaLinkedin } from "react-icons/fa";
 
 //type animation
 import { TypeAnimation } from "react-type-animation";
 
-//motion
-//import { motion } from "framer-motion";
-
-//variants
-//import { fadeIn } from "../variants";
-
 const Banner = () => {
   return (
-    <section className="section" id="home">
+    <section
+      className="min-h-[90h] lg:min-h-[60vh] flex items-center"
+      id="home"
+    >
       <div class="container mx-auto">
-        <div>
-          <h1 class="font-semibold">
-            Olá, sou Pedro <span>Palomo,</span>
-          </h1>
-          <div class="mb-6 text-[24px]  font-semibold leading">
-            <TypeAnimation
-              sequence={[
-                "Desenvolvedor Front-End",
-                2000,
-                "Desenvolvedor Back-End",
-                2000,
-                "Estudante de Programação",
-                2000,
-              ]}
-              speed={10}
-              wrapper="span"
-              repeat={Infinity}
-            />
+        <div className="flex flex-col gap-y-8 lg:flex-row lg:items-center lg:gap-x-5">
+          <div className="flex-1 text-center font-secondary lg:text-left">
+            <h1 className="text-[40px] pb-5 font-bold leading-[0.8]">
+              PEDRO <span>PALOMO</span>
+            </h1>
+            <div class="mb-6 text-[28px]  font-semibold leading-[1]">
+              <span>Sou um </span>
+              <TypeAnimation
+                sequence={[
+                  "Desenvolvedor Front-End",
+                  2000,
+                  "Desenvolvedor Back-End",
+                  2000,
+                ]}
+                speed={10}
+                wrapper="span"
+                repeat={Infinity}
+              />
+            </div>
+            <p className="mb-8 text-justify max-w-lg mx-auto lg:mx-0">
+              Desenvolvedor front-end (CSS3, HTML5, Javascript, React,
+              Bootstrap, Tailwind), back-end (Python, Node, SQL, MongoDB).
+            </p>
+            <div className="flex max-w-max gap-x-6 items-center mb-12 mx-auto lg:mx-0">
+              <a href="https://github.com/pedroplm">
+                <h2 className="text-gradient btn-link">My Portfolio</h2>
+              </a>
+            </div>
+            <div className="flex text-[20px] gap-x-6 max-w-max mx-auto lg:mx-0">
+              <a href="https://github.com/pedroplm">
+                <FaGithub />
+              </a>
+              <a href="https://github.com/pedroplm">
+                <FaFolderOpen />
+              </a>
+              <a href="https://www.linkedin.com/in/pedro-palomo-ti">
+                <FaLinkedin />
+              </a>
+            </div>
           </div>
-          <p>
-            Desenvolvedor front-end (CSS3, HTML5, Javascript, React, Bootstrap,
-            Tailwind), back-end (Python, Node, SQL, MongoDB).
-          </p>
-          <div></div>
-        </div>
-        <div>
-          <img class="rounded-full" src={Image} alt="my face" />
+          <div className="hidden lg:flex flex-1 max-w-[320px]">
+            <img class="rounded-full" src={Image} alt="my face" />
+          </div>
         </div>
       </div>
     </section>
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,26 +1,26 @@
-import React from 'react';
+import React from "react";
 //images
 import Logo from "../assets/pedro-logo-animado-bg-unscreen.gif";
 
-
 const Header = () => {
   return (
-  <header className='py-8'>
-    <div className='container mx-auto'>
-      <div className='flex justify-between items-center'>
-        {/* logo */}
-        <a href = "contact">
-          <img  
-            alt = "PT:Meu nome, Pedro Palomo, dentro de um triangulo. EN: My name, Pedro Palomo inside a black triangle" 
-            className = "w-[250px]"
-            src={Logo}
-          />
-        </a>
-        {/* button */}
-        <button className='btn btn-sm'>Trabalhe comigo</button>
+    <header className="py-8">
+      <div className="container mx-auto">
+        <div className="flex justify-between items-center">
+          {/* logo */}
+          <a to="contact">
+            <img
+              alt="PT:Meu nome, Pedro Palomo, dentro de um triangulo. EN: My name, Pedro Palomo inside a black triangle"
+              className="w-[200px]"
+              src={Logo}
+            />
+          </a>
+          {/* button */}
+          <button className="btn btn-sm">Trabalhe comigo</button>
+        </div>
       </div>
-    </div>
-  </header>)
+    </header>
+  );
 };
 
 export default Header;
